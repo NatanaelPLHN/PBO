f60282c959884b51508c272cd191b885859f85cc
NatanaelPLHN <natannaelprimuslung@gmail.com>
2024-04-03 22:54:19 +0800

    100%

---
+++ Posttest4/src/TugasPriority.java
@@ -0,0 +1,16 @@
+public class TugasPriority extends ToDoList {
+
+    public TugasPriority(int id, String judul, String deskripsi, String tenggat, boolean status){
+        super(id, judul, deskripsi, tenggat, status);
+        set_prioritas(1);
+    }
+
+    // override display
+    @Override
+    public void display() {
+        System.out.println("Tugas Prioritas!");
+        super.display();
+    }
+
+}
+
